6e0bbeb601085f680187d912db424d7f981f0c1b
H0OE <rodrigoquiroz308@gmail.com>
2022-08-08 18:33:56 -0400

    exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,58 +3,73 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular2022';
 
-  constructor(){
+  constructor() {
     const A = ['a', 'b', 'c', 'd', 'e'];
-    const auxSplice = A.splice(1,1);
-    console.log(A, auxSplice)
+    const auxSplice = A.splice(1, 1);
+    console.log(A, auxSplice);
 
-
-    const auxReduce = [1,2,3,4,5].reduce((prev, value, index)=>{
-      return prev+value
-    },0)
-    console.log(auxReduce)
+    const auxReduce = [1, 2, 3, 4, 5].reduce((prev, value, index) => {
+      return prev + value;
+    }, 0);
+    console.log(auxReduce);
 
     const auxJSON = {
-      id:1,
+      id: 1,
       name: 'Chasko',
-      lastName: 'Berto'
-    }
+      lastName: 'Berto',
+    };
 
-    const auxEntries = Object.entries(auxJSON)
-    console.log(auxEntries)
+    const auxEntries = Object.entries(auxJSON);
+    console.log(auxEntries);
 
-    const auxKeys = Object.keys(auxJSON)
-    console.log(auxKeys)
+    const auxKeys = Object.keys(auxJSON);
+    console.log(auxKeys);
 
-    const auxValues = Object.values(auxJSON)
-    console.log(auxValues)
+    const auxValues = Object.values(auxJSON);
+    console.log(auxValues);
 
-    const b = [1,2,3,4,5,6,7]
-    const c = [...b, 7,8,9,10]
-    console.log("Spread", c)
+    const b = [1, 2, 3, 4, 5, 6, 7];
+    const c = [...b, 7, 8, 9, 10];
+    console.log('Spread', c);
 
-    this.getREST(1,2,'hola')
+    this.getREST(1, 2, 'hola');
 
     const person = {
       id: 1,
-      name: "Pelo",
+      name: 'Pelo',
       country: {
         idCountry: 3,
-        cod: "Si"
-      }
-    }
+        cod: 'Si',
+      },
+    };
 
-    const {country} = person
-    console.log(country)
+    const { country } = person;
+    console.log(country);
+
+    const data = {
+      0: {
+        id: 1,
+        name: 'juan',
+        age: 24,
+      },
+      1: {
+        id: 2,
+        name: 'maria',
+        age: 10,
+      },
+    };
+
+    const auxDataValues = Object.values(data).filter(people => people.age > 10).map(people => people.id)
+    console.log(auxDataValues);
 
   }
 
-  getREST(...params: any[]){
-    console.log('rest', params)
+  getREST(...params: any[]) {
+    console.log('rest', params);
   }
 }
