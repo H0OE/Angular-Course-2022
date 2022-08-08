a6bbf2c2316d5e9cad9831a1d2ac8f5faaf90d4d
H0OE <rodrigoquiroz308@gmail.com>
2022-08-08 18:56:22 -0400

    tarea 4, componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FComponent } from '../fcomponent/fcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/fcomponent/fcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fcomponent',
+  templateUrl: './fcomponent.component.html',
+  styleUrls: ['./fcomponent.component.css']
+})
+export class FComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
