8f291f5e9379abd6062dc952ca8215492b798bd8
H0OE <rodrigoquiroz308@gmail.com>
2022-08-09 18:21:00 -0400

    1 Tarea - inputs

---
+++ src/person/person.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css'],
+})
+export class PersonComponent implements OnInit {
+  @Input() name = '';
+  @Input() job = '';
+  constructor() {}
+
+  ngOnInit() {}
+}
