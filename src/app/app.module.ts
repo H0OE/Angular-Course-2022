9115cf6f7ed198c74d01c70a463128009be0369a
H0OE <rodrigoquiroz308@gmail.com>
2022-08-18 18:35:10 -0400

    task3 - shared modules, exports, imports, declarat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { UserModule } from './user/user.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, UserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    UserModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
