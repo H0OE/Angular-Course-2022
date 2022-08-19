2c2e8221f946bd628d983102394d5c2948b290ca
H0OE <rodrigoquiroz308@gmail.com>
2022-08-19 18:45:02 -0400

    task2 - simple pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpoPipe } from './expo.pipe';
+import { PipeTestPipe } from './pipe-test.pipe';
 import { ProductComponent } from './product/product.component';
-import { Test2Component } from './test2/test2.component';
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent,
-    Test2Component
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductComponent, PipeTestPipe, ExpoPipe],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
