11713601fd56190eb61df256f0a7e07c86f3b487
H0OE <rodrigoquiroz308@gmail.com>
2022-08-23 17:54:57 -0400

    task2 -  ] custom impure pipe and pure pipe

---
+++ src/app/puree.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PureePipe } from './puree.pipe';
+
+describe('PureePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PureePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
