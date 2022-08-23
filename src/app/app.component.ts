685991e738e5c263b7ab620b6302afabe02f4e50
H0OE <rodrigoquiroz308@gmail.com>
2022-08-23 17:36:41 -0400

    task1 - pure and impure functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular2022';
+  title = 'test';
+
+  constructor() {
+    console.log('-------PURE');
+    console.log(this.pureExample(3, 5));
+    console.log(this.pureExample(1, 5));
+    console.log(this.pureExample(3, 4));
+    console.log(this.pureExample(2, 1));
+    console.log('-------IMPURE');
+    console.log(this.impureExample(3, 5));
+    console.log(this.impureExample(1, 5));
+    console.log(this.impureExample(3, 4));
+    console.log(this.impureExample(2, 1));
+  }
+
+  pureExample(a: number, b: number): number {
+    return a + b;
+  }
+
+  impureExample(a: number, b: number) {
+    return (a + b + Math.random()).toFixed();
+  }
 }
