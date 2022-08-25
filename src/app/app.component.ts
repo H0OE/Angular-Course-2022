c6659b829a25df3669df9ea8e980d9c7d5a24dff
H0OE <rodrigoquiroz308@gmail.com>
2022-08-25 18:35:54 -0400

    taask4 - sending query params from html and ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,20 @@ export class AppComponent {
   title = 'test';
 
   id: number = 1;
+  var1: any = 0;
+  var2: any = 2;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    console.log('nulish coalesing:', this.var1 ?? this.var2);
+    console.log('Or', this.var1 || this.var2);
+
+    //if(1 === 1) {
+    //  return 1 +1
+    //} else {
+    //  return 3+5
+    //}
+    console.log('ternario:', 1 === 1 ? 1 : null);
+  }
 
   goView1() {
     this.router.navigate(['view1', this.id]);
@@ -19,5 +31,10 @@ export class AppComponent {
 
   goView2() {
     this.router.navigate(['view2', this.id, 'listNames', this.id]);
+  }
+  goView3() {
+    this.router.navigate(['view3'], {
+      queryParams: { name: 'maria', lastName: this.id },
+    });
   }
 }
