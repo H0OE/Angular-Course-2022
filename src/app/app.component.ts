614ba3a99d896de5a102f26919aea22be3815e19
H0OE <rodrigoquiroz308@gmail.com>
2022-09-02 19:06:16 -0400

    task3 - DELETE and UPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,36 @@ export class AppComponent implements OnInit {
 
   constructor(private productService: ProductService) {}
   ngOnInit() {
-    this.productService.getAllProducts()
-      .subscribe(res => {
-        console.log('RESPONSE GET: ', res)
-      })
+    this.productService.getAllProducts().subscribe((res) => {
+      console.log('RESPONSE GET: ', res);
+    });
 
-    this.productService.createProduct({
-      description: 'soy la descripcion',
-      imageUrl: 'soy la imagen',
-      ownerId: 1,
-      price: 20,
-      title: 'Polera',
-    }).subscribe(res => {
-      console.log('RESPONSE POST: ', res)
-    })
+    this.productService
+      .createProduct({
+        description: 'soy la descripcion',
+        imageUrl: 'soy la imagen',
+        ownerId: 1,
+        price: 20,
+        title: 'Polera',
+      })
+      .subscribe((res) => {
+        console.log('RESPONSE POST: ', res);
+      });
+
+    this.productService.deleteProduct('p2').subscribe((res) => {
+      console.log('RESPONSE DELETE: ', res);
+    });
+
+    this.productService
+      .updateProduct('p9', {
+        description: 'modificado',
+        imageUrl: 'modificadon',
+        ownerId: 1000000,
+        price: 20000000,
+        title: 'modificado',
+      })
+      .subscribe((res) => {
+        console.log('RESPONSE UPDATE: ', res);
+      });
   }
 }
