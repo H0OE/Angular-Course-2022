41f968c85fbe4eee8313dce8369b976baff43674
H0OE <rodrigoquiroz308@gmail.com>
2022-09-05 17:49:53 -0400

    task2 - basic example reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular2022';
+export class AppComponent implements OnInit {
+  title = 'test';
+  public colorControl = new FormControl('');
+  constructor() {}
+  ngOnInit() {
+    this.colorControl.valueChanges.subscribe((value) => {
+      console.log('VALUE CHANGES: ', value);
+    });
+  }
   onSubmit(form: any) {
     console.log('FORM: ', form);
+  }
+  printColorControl() {
+    console.log(this.colorControl);
   }
 }
